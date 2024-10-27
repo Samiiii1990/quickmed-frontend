27e360bd8acc81206830c4e5645875713529aad3
Samiiii1990 <sami.hermana@gmail.com>
2024-10-27 17:39:15 -0300

    improve login css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,24 +6,32 @@ import { Router } from '@angular/router';
 @Component({  
   selector: 'app-login',  
   templateUrl: './login.component.html',  
+  styleUrls: ['./login.component.css']
 })  
 export class LoginComponent {  
-  loginForm: FormGroup;  
+  loginForm: FormGroup;
+  errorMessage: string = '';  
 
   constructor(private fb: FormBuilder, private authService: AuthService,  private router: Router ) {  
     this.loginForm = this.fb.group({  
-      email: ['', Validators.required],  
+      email: ['', [Validators.required, Validators.email]],  
       password: ['', Validators.required],  
     });  
   }  
 
   onLogin() {  
-    if (this.loginForm.valid) {  
-      this.authService.login(this.loginForm.value).subscribe(response => {  
+    if (this.loginForm.invalid) {  
+      this.errorMessage = 'Por favor ingresa tu email y contraseña correctamente';
+      return;
+    }
+
+      this.authService.login(this.loginForm.value).subscribe(response => { 
+        console.log('Usuario Logueado!');
         this.router.navigate(['/home']);
       }, error => {  
         console.error('Login error', error);  
+        this.errorMessage = 'Email o contaseña inválido';  
       });  
-    }  
+    
   }  
 }
